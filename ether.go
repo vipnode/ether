c2b013308904a9d23471b35cb7f3534765a07b3c
Andrey Petrov <andrey.petrov@shazow.net>
2018-12-19 13:54:17 -0500

    Import from vipnode codebase

---
+++ ether.go
@@ -0,0 +1,87 @@
+package ether
+
+import (
+	"fmt"
+	"math/big"
+	"strings"
+	"unicode"
+)
+
+var ethInWei = big.NewInt(1e18)
+var ethInGwei = big.NewInt(1e9)
+
+var boundary = big.NewInt(1e4)
+var gweiBoundary = new(big.Int).Div(ethInGwei, boundary)
+var ethBoundary = new(big.Int).Div(ethInWei, boundary)
+
+// Ether implements a String() formatter that dynamically adjusts to a closer
+// denomation based on the value.
+type Ether big.Int
+
+func (e Ether) String() string {
+	i := (big.Int)(e)
+	unit := "wei"
+	denom := big.NewInt(1)
+
+	if i.CmpAbs(ethBoundary) >= 0 {
+		unit = "ether"
+		denom = ethInWei
+	} else if i.CmpAbs(gweiBoundary) >= 0 {
+		unit = "gwei"
+		denom = ethInGwei
+	}
+	s := new(big.Rat).SetFrac(&i, denom).FloatString(4)
+	s = strings.TrimRight(s, "0.")
+	if s == "" {
+		s = "0"
+	}
+	return s + " " + unit
+}
+
+// ParseEther takes a string like "2 eth" and converts it to the wei equivalent.
+func ParseEther(s string) (*big.Int, error) {
+	var splitPos int
+	for pos, ch := range s {
+		if !unicode.IsNumber(ch) && ch != '-' && ch != '.' {
+			splitPos = pos
+			break
+		}
+	}
+
+	if splitPos == 0 {
+		// Must be wei
+		if r, ok := new(big.Int).SetString(s, 0); ok {
+			return r, nil
+		}
+		return nil, fmt.Errorf("failed to parse ether value: %q", s)
+	}
+
+	number, unit := s[:splitPos], s[splitPos:]
+
+	n, ok := new(big.Rat).SetString(number)
+	if !ok {
+		return nil, fmt.Errorf("failed to parse ether value: %q", s)
+	}
+
+	mul := new(big.Rat)
+	switch strings.ToLower(strings.TrimSpace(unit)) {
+	case "wei":
+	case "kwei", "babbage":
+		mul.SetInt64(1e3)
+	case "mwei", "lovelace":
+		mul.SetInt64(1e6)
+	case "gwei", "shannon":
+		mul.SetInt64(1e9)
+	case "microether", "szabo":
+		mul.SetInt64(1e12)
+	case "milliether", "finney":
+		mul.SetInt64(1e15)
+	case "ether", "eth":
+		mul.SetInt64(1e18)
+	default:
+		return nil, fmt.Errorf("failed to parse ether unit: %q", s)
+	}
+
+	n.Mul(n, mul)
+	return new(big.Int).Div(n.Num(), n.Denom()), nil
+}
---
+++ ether_test.go
@@ -0,0 +1,106 @@
+package ether
+
+import (
+	"math/big"
+	"testing"
+)
+
+func TestEther(t *testing.T) {
+	cases := []struct {
+		Amount *big.Int
+		Want   string
+	}{
+		{
+			Amount: big.NewInt(0),
+			Want:   "0 wei",
+		},
+		{
+			Amount: big.NewInt(5000000000),
+			Want:   "5 gwei",
+		},
+		{
+			Amount: big.NewInt(500000),
+			Want:   "0.0005 gwei",
+		},
+		{
+			Amount: big.NewInt(-10000000),
+			Want:   "-0.01 gwei",
+		},
+		{
+			Amount: new(big.Int).Mul(ethInWei, big.NewInt(15)),
+			Want:   "15 ether",
+		},
+	}
+
+	for i, tc := range cases {
+		got := Ether(*tc.Amount).String()
+		if got != tc.Want {
+			t.Errorf("case #%d: got: %q; want %q", i, got, tc.Want)
+		}
+	}
+}
+
+func TestParseEther(t *testing.T) {
+	cases := []struct {
+		Input   string
+		Want    *big.Int
+		IsError bool
+	}{
+		{
+			Want:  big.NewInt(0),
+			Input: "0",
+		},
+		{
+			Want:  big.NewInt(42),
+			Input: "42",
+		},
+		{
+			Want:  big.NewInt(0),
+			Input: "0 wei",
+		},
+		{
+			Want:  big.NewInt(5000000000),
+			Input: "5 gwei",
+		},
+		{
+			Want:  big.NewInt(500000),
+			Input: "0.0005 gwei",
+		},
+		{
+			Want:  big.NewInt(-10000000),
+			Input: "-0.01 gwei",
+		},
+		{
+			Want:  new(big.Int).Mul(ethInWei, big.NewInt(15)),
+			Input: "15 ether",
+		},
+		{
+			Input:   "",
+			IsError: true,
+		},
+		{
+			Input:   "foo",
+			IsError: true,
+		},
+		{
+			Input:   "1 foo",
+			IsError: true,
+		},
+		{
+			Input:   "- eth",
+			IsError: true,
+		},
+	}
+
+	for i, tc := range cases {
+		got, err := ParseEther(tc.Input)
+		if tc.IsError && err != nil {
+			continue
+		}
+		if (err != nil) != tc.IsError {
+			t.Errorf("case #%d: got error: %v; wanted IsError=%t", i, err, tc.IsError)
+		} else if got.Cmp(tc.Want) != 0 {
+			t.Errorf("case #%d: got: %q; want %q (input: %q)", i, got, tc.Want, tc.Input)
+		}
+	}
+}
